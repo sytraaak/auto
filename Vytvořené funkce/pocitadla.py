5e31f83028c4d2d60d4a5cf29f423c46a6c05730
Jan Burda <burdajen@gmail.com>
2021-03-03 18:35:26 +0100

    Funkce počítadel

---
+++ Vytvořené funkce/pocitadla.py
@@ -0,0 +1,25 @@
+def pocet_znaku(a):
+  """
+  Sečte znaky a vrátí jejich počet ve slovniku znak : pocet
+  """
+  pocet = dict()
+  for char in a.lower():
+    if char not in pocet.keys():
+      pocet[char] = 1
+    else:
+      pocet[char] += 1
+  return print(pocet)
+
+def pocet_prvku(a):
+  """
+  Sečte počet prvků v tuplu, listu nebo slovníku
+  U slovníku vrací klíče
+  """
+  pocet = dict()
+  for char in a:
+    if char not in pocet.keys():
+      pocet[char] = 1
+    else:
+      pocet[char] += 1
+  return print(pocet)
+
